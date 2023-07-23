0f4be0e8a9e65d328380d86ac56365db2965a248
Olga Mirete <olgamirete@gmail.com>
2023-07-23 16:41:40 +0200

    referenced env variable

--- server/api/models.get.ts
+++ server/api/models.get.ts
@@ -2,8 +2,7 @@ export default defineEventHandler(async (event) => {
     const url = "https://api.openai.com/v1/models";
     const response = await fetch(url, {
         headers: {
-            Authorization:
-                "Bearer ***REMOVED***",
+            Authorization: `Bearer ${process.env.OPENAI_API_SECRET}`,
         },
     });
     return await response.json();

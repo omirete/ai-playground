09e9f72e21ba87132c0113365f9cd9ad95f013c9
Olga Mirete <olgamirete@gmail.com>
2023-08-21 21:56:41 +0200

    added SubmitButton component

---
+++ src/components/ui/SubmitButton/index.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import Button from "primevue/button";
+import ProgressSpinner from "primevue/progressspinner";
+const { loading, text } = defineProps<{ loading: boolean; text: string }>();
+</script>
+
+<template>
+    <Button type="submit" :disabled="loading">
+        <div class="flex gap-2 align-items-center">
+            <span>{{ text }}</span>
+            <ProgressSpinner
+                v-if="loading"
+                style="height: 1em; width: 1em"
+                stroke-width="10"
+                aria-label="Loading"
+            />
+        </div>
+    </Button>
+</template>

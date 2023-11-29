66912ed3c06ef5b779dab5d1c0328cf3f4e07b0f
Olga Mirete <olgamirete@gmail.com>
2023-11-29 17:49:20 -0300

    avoid duplicating code

--- config/config.js
+++ config/config.js
@@ -1,20 +1,10 @@
 require("dotenv").config();
-const mysql2 = require("mysql2");
-
-module.exports = {
-    username: process.env.DB_USR,
-    password: process.env.DB_PWD,
-    database: process.env.DB_NAME,
-    host: process.env.DB_HOST,
-    port: parseInt(process.env.DB_PORT ?? ""),
-    dialect: process.env.DB_DIALECT,
-    dialectModule: mysql2,
-    ssl: true,
-    dialectOptions: {
-        ssl: {
-            key: process.env.DB_PRIVATE_KEY.replaceAll("\\n", "\n"),
-            cert: process.env.DB_PUBLIC_KEY.replaceAll("\\n", "\n"),
-            ca: process.env.DB_PUBLIC_KEY.replaceAll("\\n", "\n"),
-        },
+require("ts-node").register({
+    transpileOnly: true,
+    compilerOptions: {
+        module: "commonjs",
     },
-};
+});
+const { sequelizeOpts } = require("../src/helpers/DB.ts"); // Change the path to your config file
+
+module.exports = sequelizeOpts;
--- src/helpers/DB.ts
+++ src/helpers/DB.ts
@@ -1,6 +1,8 @@
 import { Dialect, Options, Sequelize } from "sequelize";
 import mysql2 from "mysql2";
 
+export const dbSSL: boolean = process.env.DB_PRIVATE_KEY !== undefined
+
 export const sequelizeOpts: Options = {
     username: process.env.DB_USR,
     password: process.env.DB_PWD,
@@ -9,13 +11,13 @@ export const sequelizeOpts: Options = {
     port: parseInt(process.env.DB_PORT ?? ""),
     dialect: process.env.DB_DIALECT as Dialect,
     dialectModule: mysql2,
-    ssl: true,
+    ssl: dbSSL,
     dialectOptions: {
-        ssl: {
+        ssl: dbSSL ? {
             key: (process.env.DB_PRIVATE_KEY as string).replaceAll("\\n", "\n"),
             cert: (process.env.DB_PUBLIC_KEY as string).replaceAll("\\n", "\n"),
             ca: (process.env.DB_PUBLIC_KEY as string).replaceAll("\\n", "\n"),
-        },
+        } : undefined,
     },
 };
 

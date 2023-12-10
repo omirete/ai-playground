8e2868e3b55bbfc3f50b165d898a65c51d275b26
Olga Mirete <olgamirete@gmail.com>
2023-12-10 18:52:52 -0300

    removed unneeded config

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -7,14 +7,5 @@ module.exports = {
             instances: "max",
             script: "./.output/server/index.mjs",
         },
-    ],
-    env: {
-        "OPENAI_ORG_ID": process.env.OPENAI_ORG_ID,
-        "OPENAI_API_SECRET": process.env.OPENAI_API_SECRET,
-        "DB_CONNECTION_STRING": process.env.DB_CONNECTION_STRING,
-        "IMG_SAVE_DIR": process.env.IMG_SAVE_DIR,
-        "IMG_BASE_URL": process.env.IMG_BASE_URL,
-        "PRIVATE_KEY": process.env.PRIVATE_KEY,
-        "PUBLIC_KEY": process.env.PUBLIC_KEY
-    }
+    ]
 };

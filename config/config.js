3c8ffb6ef60b7fb7d6554a4bd83bd9b62e285a0f
Olga Mirete <olgamirete@gmail.com>
2023-12-10 20:27:12 -0300

    updated DB configs

--- config/config.js
+++ config/config.js
@@ -1,10 +1,6 @@
 require("dotenv").config();
-require("ts-node").register({
-    transpileOnly: true,
-    compilerOptions: {
-        module: "commonjs",
-    },
-});
-const { sequelizeOpts } = require("../src/helpers/DB.ts"); // Change the path to your config file
 
-module.exports = sequelizeOpts;
+module.exports = {
+    url: process.env.DB_CONNECTION_STRING,
+    dialectModule: require('mysql2'),
+}
--- src/helpers/DB.ts
+++ src/helpers/DB.ts
@@ -1,37 +1,10 @@
-import { type Dialect, type Options, Sequelize } from "sequelize";
+import { type Options, Sequelize } from "sequelize";
 import mysql2 from "mysql2";
-
-export const dbSSL: boolean = process.env.DB_PRIVATE_KEY !== undefined;
 
 const connection_string: string = process.env.DB_CONNECTION_STRING as string;
 
 export const sequelizeOpts: Options = {
-    // username: process.env.DB_USR,
-    // password: process.env.DB_PWD,
-    // database: process.env.DB_NAME,
-    // host: process.env.DB_HOST,
-    // port: parseInt(process.env.DB_PORT ?? ""),
-    // dialect: process.env.DB_DIALECT as Dialect,
     dialectModule: mysql2,
-    ssl: dbSSL,
-    dialectOptions: {
-        ssl: dbSSL
-            ? {
-                  key: (process.env.DB_PRIVATE_KEY as string).replaceAll(
-                      "\\n",
-                      "\n"
-                  ),
-                  cert: (process.env.DB_PUBLIC_KEY as string).replaceAll(
-                      "\\n",
-                      "\n"
-                  ),
-                  ca: (process.env.DB_PUBLIC_KEY as string).replaceAll(
-                      "\\n",
-                      "\n"
-                  ),
-              }
-            : undefined,
-    },
 };
 
 export const sequelize = new Sequelize(connection_string, sequelizeOpts);

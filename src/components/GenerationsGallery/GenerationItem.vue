85ed9568e6ddf4655b6122fc12c4474430d3ddd9
Olga Mirete <olgamirete@gmail.com>
2023-12-22 17:57:05 -0300

    extracted component

---
+++ src/components/GenerationsGallery/GenerationItem.vue
@@ -0,0 +1,42 @@
+<script setup lang="ts">
+import getImgSrc from "@/src/helpers/getImgSrc";
+import Icon from "@/src/components/ui/Icon/index.vue";
+import type GeneratedImage from "@/src/types/GeneratedImage";
+
+const props = defineProps<{ image: GeneratedImage }>();
+</script>
+
+<template>
+    <div
+        class="d-flex align-items-start p-1 m-0 mt-2 me-2 bg-light-hover rounded cursor-pointer"
+    >
+        <img
+            class="shadow-sm rounded"
+            :src="getImgSrc(props.image.image)"
+            :alt="props.image.prompt"
+            width="80"
+            height="80"
+            loading="lazy"
+        />
+        <div class="flex-grow-1 d-flex flex-column ms-2">
+            <div class="text-dark">
+                {{ props.image.prompt }}
+            </div>
+            <div class="d-flex align-items-center text-black-50">
+                <Icon icon-name="calendar" />
+                <span class="ms-1">{{
+                    new Date(props.image.createdAt).toLocaleDateString(
+                        undefined,
+                        {
+                            year: "numeric",
+                            month: "2-digit",
+                            day: "2-digit",
+                        },
+                    )
+                }}</span>
+                <Icon icon-name="box" class="ms-2" />
+                <span class="ms-1">{{ props.image.model }}</span>
+            </div>
+        </div>
+    </div>
+</template>
--- src/components/GenerationsGallery/index.vue
+++ src/components/GenerationsGallery/index.vue
@@ -1,42 +1,19 @@
 <script setup lang="ts">
-import getImgSrc from "@/src/helpers/getImgSrc";
-import Icon from "@/src/components/ui/Icon/index.vue";
 import type GeneratedImage from "@/src/types/GeneratedImage";
+import GenerationItem from "./GenerationItem.vue";
 
-const { imageGenerations, onSelect } = defineProps<{
+const props = defineProps<{
     imageGenerations: GeneratedImage[];
     onSelect: (generation: GeneratedImage) => void;
 }>();
 </script>
 
 <template>
-    <div
-        v-for="image in imageGenerations"
-        class="d-flex align-items-start p-1 m-0 mt-2 me-2 bg-light-hover rounded cursor-pointer"
-        @click="() => onSelect(image)"
-    >
-        <img
-            class="shadow-sm rounded"
-            :src="getImgSrc(image.image)"
-            :alt="image.prompt"
-            width="80"
-            height="80"
-            loading="lazy"
+    <div class="position-relative">
+        <GenerationItem
+            v-for="image in props.imageGenerations"
+            :image="image"
+            @click="() => props.onSelect(image)"
         />
-        <div class="flex-grow-1 d-flex flex-column ms-2">
-            <div class="text-dark">
-                {{ image.prompt }}
-            </div>
-            <div class="d-flex align-items-center text-black-50">
-                <Icon icon-name="calendar" />
-                <span class="ms-1">{{
-                    new Date(image.createdAt).toLocaleDateString(undefined, {
-                        year: "numeric",
-                        month: "2-digit",
-                        day: "2-digit",
-                    })
-                }}</span>
-            </div>
-        </div>
     </div>
 </template>

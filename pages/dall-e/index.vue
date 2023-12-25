5b0afdba3817b10b27f7074ebbedbe8733f20ed0
Olga Mirete <olgamirete@gmail.com>
2023-12-25 10:50:03 -0300

    relocated client components in the components folder

--- pages/dall-e/index.vue
+++ pages/dall-e/index.vue
@@ -3,9 +3,9 @@ import PrivateSection from "@/src/components/PrivateSection/index.vue";
 
 import GeneratedImagesContextProvider from "@/src/contexts/GeneratedImagesContextProvider/index.vue";
 
-import Preview from "./Preview.vue";
-import DalleForm from "./DalleForm.vue";
-import GalleryPrevious from "./GalleryPrevious.vue";
+import Preview from "@/src/components/pages/dall-e/Preview.vue";
+import DalleForm from "@/src/components/pages/dall-e/DalleForm.vue";
+import GalleryPrevious from "@/src/components/pages/dall-e/GalleryPrevious.vue";
 </script>
 
 <template>
